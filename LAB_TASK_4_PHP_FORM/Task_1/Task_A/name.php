b0284c5360ca141f1b065353e4ec17a59a0c679b
mdashikurrahman581 <84652681+mdashikurrahman581@users.noreply.github.com>
2021-06-15 19:29:09 +0600

    Add files via upload

---
+++ LAB_TASK_4_PHP_FORM/Task_1/Task_A/name.php
@@ -0,0 +1,17 @@
+<?php
+	if(isset($_POST['submit'])){
+
+		$name 		=  $_POST['myname'];
+
+		if($name == ""){
+			echo "Null submission...";
+		}else{
+			echo "Name: ".$_POST['myname'];
+		}
+
+	}else{
+		echo 'invalid request...';
+	}
+
+
+?>
