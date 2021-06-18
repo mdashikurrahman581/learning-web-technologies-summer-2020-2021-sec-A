585dcfdcfff79dfe0c9f0cb97ab00dab7d6b43f5
Md. Ashikur Rahman <mdashikurrahman581@gmail.com>
2021-06-18 16:33:38 +0600

    fc

---
+++ LAB_TASK_4_PHP_FORM/Task_2/Task_B/email1.php
@@ -0,0 +1,33 @@
+<?php
+
+if(isset($_POST['submit'])){
+
+    $email 		=  $_POST['email'];
+
+    if($email == ""){
+        echo "Null submission...";
+    }else{
+        echo $email;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Email Input field</title>
+</head>
+
+<body>
+    <form method="POST" action="#">
+        <fieldset>
+            <legend>EMAIL</legend>
+            <input type="email" name="email" value=""> <br>
+            <hr>
+            <input type="submit" name="submit" value="Submit">
+        </fieldset>
+    </form>
+</body>
+
+</html>
