65a2afdd844f3ba9c12aef4da79fc6f2f77c0d80
Md. Ashikur Rahman <mdashikurrahman581@gmail.com>
2021-06-20 23:38:24 +0600

    forgot pass

---
+++ LAB_TASK_5_SESSION/forgotpass.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index Page</title>
+</head>
+<body>
+	<center>
+		<a href="home.php">Home</a> |
+		<a href="login.php">Login</a>|
+		<a href="Registration.php">Registration</a>
+
+	
+
+	</center>
+</body>
+</html>
+
+
+
+
+
+<html>
+<head>
+	<title>Forgot Password</title>
+</head>
+<body>
+	<fieldset>
+		<legend>Forgot Password</legend>
+		<form method="POST" action="forgotpass.php">
+
+		<form method="POST" action="forgotpass.php">
+
+<table >
+			<tr>
+					<td>Email </td>
+					<td><input type="text" name="Email" value="" placeholder="type email @gmail.com"></td>
+				</tr>
+
+					<tr>
+					<td></td>
+					<td>
+					<hr></hr>
+						
+						     <input type="submit" name="submit"> 
+            <br>
+            <br>
+					</td>
+				</tr>
+</body>
+			
+</table>
+</form>
+	</fieldset>
+
+	</form>
+	
+
+</html>
