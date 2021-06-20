0bc0647fb52cde7d619447d5ab018d73be3d1651
Md. Ashikur Rahman <mdashikurrahman581@gmail.com>
2021-06-20 23:28:15 +0600

    dash board

---
+++ LAB_TASK_5_SESSION/loginInaDashboard.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Log in in a dashboard</title>
+</head>
+<body>
+	<center>
+		<a href="home.php">Home</a> |
+		<a href="login.php">Login</a>|
+		<a href="Registration.php">Registration</a>
+
+		<h1>Welcome ! </h1>
+
+        <a href="viewprofile.php">View Profile</a> <br/>
+		<a href="editprofile.php">Edit Profile</a><br/>
+		<a href="profilePicture.php"> Change Profile Picture</a><br/>
+		<a href="changepass.php">Change Password</a>
+
+
+
+	</center>
+</body>
+</html>
